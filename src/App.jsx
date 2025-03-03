81f6f1ffa446974b1ad155d21ab50ef8732e953a
Vineet Somani <vineet.comfygen@gmail.com>
2025-03-03 19:32:00 +0530

    done3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
